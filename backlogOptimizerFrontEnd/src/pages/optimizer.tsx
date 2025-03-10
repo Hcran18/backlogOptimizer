1af49eae0bbbfa9c14acc0f64092a9e4c73d7c9a
Hcran18 <hunter.crandall18@gmail.com>
2025-03-10 16:48:15 -0600

    update error handling

--- backlogOptimizerFrontEnd/src/pages/optimizer.tsx
+++ backlogOptimizerFrontEnd/src/pages/optimizer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Preferences from '@/components/preferences';
 import GamesForm from '@/components/gamesForm';
 import InstructionsDialog from '@/components/instructionsDialog';
